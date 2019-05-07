12224ae90805625f2f9bbdb48fa4647cec01daaf
johnguiscool <johnguiscool@gmail.com>
2019-05-07 11:40:36 -0400

    mastering magento

---
+++ app/code/local/MasteringMagento/Example/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class MasteringMagento_Example_Helper_data extends Mage_Core_Helper_Abstract
+{
+    //empty helper class
+}
---
+++ app/code/local/MasteringMagento/Example/controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+class MasteringMagento_Example_HelloController extends Mage_Core_Controller_Front_Action
+{
+    public function worldAction()
+    {
+        echo 'Hello, World!';
+    }
+
+    public function indexAction()
+    {
+        echo 'Hello, World!';
+    }
+}
